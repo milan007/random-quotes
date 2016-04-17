cf87cd9330009db28ee36b215c393036f7565024
milan007 <milanvukosavljevic@hotmail.com>
2016-04-17 19:21:24 -0400

    Added a reset all settings option
    Added a reset all settings preference. It will reset all the settings to
    their default values

---
+++ app/src/main/java/com/mentatmobile/randomquotes/ResetDefaultsPreference.java
@@ -0,0 +1,30 @@
+package com.mentatmobile.randomquotes;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.preference.DialogPreference;
+import android.preference.PreferenceManager;
+import android.util.AttributeSet;
+
+public class ResetDefaultsPreference extends DialogPreference {
+    protected Context context;
+
+    public ResetDefaultsPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        super.onClick(dialog, which);
+
+        if(which == DialogInterface.BUTTON_POSITIVE) {
+            SharedPreferences.Editor preferencesEditor = PreferenceManager.getDefaultSharedPreferences(this.context).edit();
+            preferencesEditor.clear();
+            PreferenceManager.setDefaultValues(context, R.xml.preferences, true);
+            preferencesEditor.commit();
+            getOnPreferenceChangeListener().onPreferenceChange(this, true);
+        }
+    }
+}
--- app/src/main/java/com/mentatmobile/randomquotes/SettingsActivity.java
+++ app/src/main/java/com/mentatmobile/randomquotes/SettingsActivity.java
@@ -1,6 +1,9 @@
 package com.mentatmobile.randomquotes;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 
@@ -12,11 +15,34 @@ public class SettingsActivity extends PreferenceActivity {
     }
 
     public static class SettingsFragment extends PreferenceFragment {
+
         @Override
         public void onCreate(final Bundle savedInstanceState){
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
-        }
-    }
 
+            Preference resetDialogPreference = getPreferenceScreen().findPreference("resetDialog");
+            resetDialogPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    Activity activity = getActivity();
+                    Intent startIntent = activity.getIntent();
+
+                    //Both enter and exit animations are set to zero, so no transition animation is applied
+                    activity.overridePendingTransition(0, 0);
+                    //Call this line, just to make sure that the system doesn't apply an animation
+                    getActivity().getIntent().addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    //Close this Activity
+                    activity.finish();
+                    //Again, don't set an animation for the transition
+                    activity.overridePendingTransition(0, 0);
+                    //Start the activity by calling the Intent that have started this same Activity
+                    startActivity(startIntent);
+                    //Return false, so that nothing happens to the preference values
+                    return false;
+                }
+            });
+        }
+
+    }
 }
